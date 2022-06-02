5b1f6fdea39480a8f0b699fd6807c527fb903414
AnkitMahalpure <75879710+AnkitMahalpure@users.noreply.github.com>
2022-06-02 10:24:12 +0530

    button

--- resumebuilder/src/Components/Landing.js
+++ resumebuilder/src/Components/Landing.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import '../Landing.css'
+import {authentication} from '../Firebase/firebase'
+import {signInWithPopup , GoogleAuthProvider} from 'firebase/auth';
 export default function Landing() {
+
+  const SignInWithFirebase = ()=>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication,provider)
+    .then((re)=> {
+      //console.log(userInfo);
+        var userInfo = re;
+        //console.log(re.user.displayName
+        //console.log(re)
+    })
+    .catch((err)=> {
+      console.log(err)
+    })
+  }
   return (<>
-  <div>Landing</div>
+  <div>
+    <h1>Egg Seller</h1>
+    <button className='buy' onClick={SignInWithFirebase}>Buy Eggs</button>
+  </div>
   </>
   )
 }
