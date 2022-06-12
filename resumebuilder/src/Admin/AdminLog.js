fba40cb12aad15f9def1085fdbf9d405468f9842
Roahn <72019287+Roahn@users.noreply.github.com>
2022-06-12 12:20:39 +0530

    commented trial testing form in admin page

--- resumebuilder/src/Admin/AdminLog.js
+++ resumebuilder/src/Admin/AdminLog.js
@@ -2,23 +2,29 @@ import React from 'react';
 import './Admin.css';
 import Nav from '../Components/Nav';
 import { Link } from 'react-router-dom';
+import DashboardData  from './DashboardData';
 export default function AdminLog(props) {
   const profile = props.title;
 
   return (
     <>
       <Nav Info={profile}></Nav>
-      <form method='POST' action='/addTweet'>
+      {/* <form method='POST' action='/addTweet'>
         <div class='input-group justify-content-center'>
           <div class='input-group-prepend'>
             <input type='text' name='tweetInput' class='form-control' />
             <input type='submit' value='Send' class='btn btn-primary mb-2' />
           </div>
         </div>
-      </form>
+      </form> */}
 
-      <br />
+      {/* <br /> */}
       <h1 id='AdminTitle'>Admin Page</h1>
+      <section className='Dashsection'>
+             <DashboardData>
+    </DashboardData>
+      </section>
+ 
     </>
   );
 }
