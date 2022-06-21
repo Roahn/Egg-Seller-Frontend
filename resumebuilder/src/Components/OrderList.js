d9aec3c104acd02e0402fe8131916cca3e835b9d
Roahn <72019287+Roahn@users.noreply.github.com>
2022-06-21 22:11:43 +0530

    Created MyOrders page

--- resumebuilder/src/Components/OrderList.js
+++ resumebuilder/src/Components/OrderList.js
@@ -1,7 +1,23 @@
-import React from 'react'
 
-export default function OrderList() {
-  return (
-    <div>OrderList</div>
-  )
+import React, { useEffect } from 'react';
+import MyOrder from './MyOrder';
+export default function OrderList({ user}) {
+    useEffect(() => {
+      fetchItems();
+    }, []);
+    const [items, setItems] = React.useState([]);
+
+    const fetchItems = async () => {
+      const data = await fetch(`/UserOrderData?uid=${user.uid}`);
+      const items = await data.json(data);
+      setItems(items);
+    };
+
+    //console.log(items);
+    return (
+      <>
+      <MyOrder user={user}></MyOrder>
+  
+      </>
+    );
 }
