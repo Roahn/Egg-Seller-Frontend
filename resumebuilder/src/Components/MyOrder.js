61429e2aa2497b2a8a6e444558f7f56dc834cbcf
Roahn <72019287+Roahn@users.noreply.github.com>
2022-06-22 11:05:16 +0530

    New orders in myorder page will be added to top of the list

--- resumebuilder/src/Components/MyOrder.js
+++ resumebuilder/src/Components/MyOrder.js
@@ -56,11 +56,11 @@ function getComparator(order, orderBy) {
 function stableSort(array, comparator) {
   const stabilizedThis = array.map((el, index) => [el, index]);
   stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
+    const order = comparator(b[0], a[0]);
     if (order !== 0) {
       return order;
     }
-    return a[1] - b[1];
+    return b[1] - a[1];
   });
   return stabilizedThis.map((el) => el[0]);
 }
